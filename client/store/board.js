a1ce011214a2ec4344bd4065558a7bc5a1711d60
Georgina Hoagland <georgina.hoagland@gmail.com>
2017-11-11 13:38:27 -0500

    finished setting up board and card generation

--- client/store/board.js
+++ client/store/board.js
@@ -4,15 +4,20 @@ const GET_NEW_BOARD = 'GET_NEW_BOARD'
 /*----------ACTION CREATORS----------*/
 const getNewBoard = (board) => ({type: GET_NEW_BOARD, board})
 
-/*----------HELPER FUNCTIONS ----------*/
-export function generateBoard(numCards=9, currentBoard=[]) {
-  for (let i = 0; i < numCards - 1; i++) {
-      currentBoard.push()
+/*----------THUNK CREATORS/HELPER FUNCTIONS ----------*/
+export function generateBoard(numCards = 9, currentBoard = []) {
+  return function thunk (dispatch) {
+    for (let i = 0; i < numCards - 1; i++) {
+      currentBoard.push(generateRandomCard());
+    }
+    // ensures there is at least one set
+    const newBoard = generateLastCard(currentBoard);
+    dispatch(getNewBoard(newBoard));
+    return newBoard
   }
-
 }
 
-function generateRandomNum(min=0, max=2) {
+function generateRandomNum(min = 0, max = 2) {
   return (Math.round(max - min) * Math.random() + min)
 }
 
@@ -38,10 +43,11 @@ function generateLastCard(currentBoard) {
   const card2 = currentBoard[idx2];
 
   //generates a card to finish the set
+  const newCard = completeSet(card1, card2);
 
   //inserts the card at a random location in the board
-
-
+  const insertIdx = generateRandomNum(0, currentBoard.length - 1);
+  return [...currentBoard.slice(0, insertIdx), newCard, ...currentBoard.slice(insertIdx)];
 }
 
 function completeSet(card1, card2) {
@@ -54,10 +60,15 @@ function completeSet(card1, card2) {
       newCard.push((((card1Prop + card2Prop) * 2) % 3 + 3) % 3)
     }
   })
-  return newCard
+  return newCard;
 }
 /*----------REDUCER----------*/
 
-export default function reducer (initialState=[], action) {
-
+export default function reducer (initialState = [], action) {
+  switch (action.type) {
+    case GET_NEW_BOARD:
+      return action.board;
+    default:
+      return initialState
+  }
 }
